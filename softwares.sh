5b60691cdc90e90c97667267fb079e6ced97bdde
joco55612 <joco55612@gmail.com>
2017-03-31 14:17:24 -0400

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -9,9 +9,9 @@ sudo pacman -S libreoffice inkscape gimp blender atom pcmanfm mpv youtube-dl qbi
 
 source <(curl -s $SERVER/atom.sh)
 source <(curl -s $SERVER/gvim.sh)
-source <(curl -s $SERVER/speedarch.sh)
 source <(curl -s $SERVER/virtualbox.sh)
 source <(curl -s $SERVER/samba.sh)
+source <(curl -s $SERVER/speedarch.sh)
 
 echo "-- reboot --" && read a
 reboot
