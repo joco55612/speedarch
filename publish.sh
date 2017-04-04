a70b56ac213641f4be12dcda9d8039eaa4415ace
joco55612 <joco55612@gmail.com>
2017-04-04 03:34:38 -0400

    publix fix (1)

--- publish.sh
+++ publish.sh
@@ -22,9 +22,13 @@ cd speedarch
 IP="$(ip addr show |grep "inet " |grep -v 127.0.0. |head -1|cut -d" " -f6|cut -d/ -f1)"
 YOUR_RAW=${YOUR_RAW//\//\\/}
 find . -type f -exec sed -i -e "s/http:\/\/$IP\/speedarch/$YOUR_RAW/g" {} \;
-source save.sh
 
-cd /tmp/speedarch/
+cd /tmp
+rm -rf speedarch/speedarch.tar.gz
+tar -zcvf speedarch.tar.gz speedarch/
+mv speedarch.tar.gz speedarch/speedarch.tar.gz
+
+cd speedarch
 git add .
 echo "Your commit message" && read MESSAGE
 git commit -m "$MESSAGE"
